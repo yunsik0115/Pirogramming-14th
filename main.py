5f7457d8737a7df73c60b21a3d50af714c6c4b58
Yunsik Kim <pubyunsik0115@gmail.com>
2020-12-04 21:27:45 +0900

    Thanks To.
    https://pandas.pydata.org/pandas-docs/version/0.13.1/indexing.html
    https://stackoverflow.com/questions/29576430/shuffle-dataframe-rows
    
    ----------------------------
    문제 내용 잘못 이해하여 수정.
    5회 실행 -> 1회만 사용자 지정 시행 -> 나머지는 무작위 숫자를 골라 작업 시행
    캡슐화, 정보은닉 (객체지향 특성)
     -> 데이터테이블 클래스 내부로 이동.
    추상화가 잘 안되는듯 함.
    간결하게 바꿔보자
    -----------------------------
    Shuffling 이후 치료 국가 출력부분 오류
    -> nation_ind 리스트는 변화하지 않아서 발생. 해결 요함.
    (원래 한국 == index 1 ,독일 == index 2 일때 완치국가 index ==1일 경우 shuffling 이후 위 아래 순서 바뀌면 완치되지 않은 독일이 출력되는 문제 해결 요망)

--- main.py
+++ main.py
@@ -1,73 +1,74 @@
 import pandas as pd
+import numpy as np
+import random
 import time
-
-nation_col = ['인구 수', '감염자 수']
-nation_ind = ['1. 한국', '2. 중국', '3. 일본', '4. 미국', '5. 독일']
-nation_con = [[1500, 300], [3000, 800], [2000, 500], [2500, 750], [2200, 1000]]
-nation_table = pd.DataFrame(nation_con, columns=nation_col, index=nation_ind)
-nation_table.con.astype(int)
-
-vac_col = ['백신치료율(단위 %)']
-vac_ind = ['백신 1', '백신 2', '백신 3']
-vac_con = [25, 50, 100]
-vac_table = pd.DataFrame(vac_con, columns=vac_col, index=vac_ind)
 
 
 class Services:
+    nation_col = ['인구 수', '감염자 수']
+    nation_ind = ['한국', '중국', '일본', '미국', '독일']
+    nation_con = [[1500, 300], [3000, 800], [2000, 500], [2500, 750], [2200, 1000]]
+    nation_table = pd.DataFrame(nation_con, columns=nation_col, index=nation_ind)
+
+    vac_col = ['백신치료율(단위 %)']
+    vac_ind = ['백신 1', '백신 2', '백신 3']
+    vac_con = [25, 50, 100]
+    vac_table = pd.DataFrame(vac_con, columns=vac_col, index=vac_ind)
     round = 0
 
     def print_vac(self):
         print("========백신 목록=========")
-        print(vac_table)
+        print(self.vac_table)
 
     def cure(self, nation, vaccine):
-        print("★", self.round + 1, "번째 시도★")
+        print("\n\n\n★", self.round + 1, "번째 시도★")
         print("=============선택된 백신 정보=============\n",
-              vac_table.iloc[[vaccine - 1]])
+              self.vac_table.iloc[[vaccine - 1]])
         print("=============선택된 국가 정보=============\n",
-              nation_table.iloc[[nation - 1]])
-        nation_table.iloc[nation - 1, 1] *= (1 - vac_table.iloc[vaccine - 1, 0] * 0.01)
+              self.nation_table.iloc[[nation - 1]])
+        self.nation_table.iloc[nation - 1, 1] *= (1 - self.vac_table.iloc[vaccine - 1, 0] * 0.01)
 
     def infection_increase(self, nation):
         index = 0
         while index < 5:
-            if nation == index:
-                continue
-            nation_table.iloc[index, 1] += nation_table.iloc[index, 1] * 0.25
+            self.nation_table.iloc[index, 1] += self.nation_table.iloc[index, 1] * 0.15
             index += 1
         self.round += 1
 
     def is_finished(self):
         index = 0
         while index < 5:
-            if nation_table.iloc[index, 0] < nation_table.iloc[index, 1]:
+            if self.nation_table.iloc[index, 0] < self.nation_table.iloc[index, 1]:
                 print("감염자수가 인구수보다 증가함에 따라 게임을 종료합니다")
                 self.print_score()
-                return 1
+                return True
             index += 1
 
         if self.round > 4:
             print("5라운드까지 모두 종료되었습니다. 결과를 출력합니다")
             self.print_score()
-            return 1
+            return True
 
     def print_result(self):
+        index = 0
         if self.round != 0:
+            print("\n", self.round, '차백신투여 후 감염된 나라에 대한 정보')
             print("============================================")
-            print(self.round, '차백신투여 후 감염된 나라에 대한 정보')
-            print("============================================")
-        print("========국가 목록=========\n", nation_table, "\n")
+            while index < 5:
+                if self.nation_table.iloc[index, 1] == 0:
+                    print("완치된 국가 : ", self.nation_ind[index])
+                index += 1
+        print("========국가 목록=========\n", self.nation_table, "\n")
 
     def print_score(self):
         print("=======최종 스코어=======")
-        final_score = nation_table.sort_values(by='감염자 수', ascending=True)
+        final_score = self.nation_table.sort_values(by='감염자 수', ascending=True)
         print(final_score)
         print("========출력 종료========")
 
     def shuffle(self):
-        nation_table.sample(frac=1).reset_index(drop=True)
-        self.round = 0
-
+        self.nation_table = self.nation_table.sample(frac=1)
+        self.vac_table = self.vac_table.sample(frac=1)
 
 while True:
     print('''
@@ -95,17 +96,19 @@ while True:
         Service.print_vac()
         Service.print_result()
         vaccine_choice = int(input("사용할 백신 선택(1~3) : "))
-        nation_choice = list(map(int, input("국가 선택(0또는 미입력시 랜덤) : ").split()))
-        nation_choice = nation_choice + [0] * (5 - len(nation_choice))
+        nation_choice = int(input("적용할 국가 선택(1~5) : "))
         while True:
-            Service.cure(nation_choice[i], vaccine_choice)
-            Service.infection_increase(nation_choice[i])
+            Service.cure(nation_choice, vaccine_choice)
+            Service.infection_increase(nation_choice)
             Service.print_result()
             ch = Service.is_finished()
-            if ch == 1:
+            if ch:
+                time.sleep(3)
                 break
+            vaccine_choice = random.randint(1, 3)
+            nation_choice = random.randint(1, 5)
+            Service.shuffle()
             time.sleep(3)
-            i += 1
     elif choice == 4:
         print("게임을 종료합니다")
         break
