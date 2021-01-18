be013baf121c633e8c284b1fc216104688fd25f8
Yunsik Kim <pubyunsik0115@gmail.com>
2021-01-18 22:48:57 +0900

    21강

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = '6t!qstskly(s1x_e#f%4^i6jgc=e^=c)!^dw)m!vonuz1x-une'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -60,7 +61,9 @@ ROOT_URLCONF = 'askcompany.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'askcompany', 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +129,35 @@ USE_TZ = True
 STATIC_URL = '/static/'
 INTERNAL_IPS = ['127.0.0.1',
                 ]
+
+LOGGING = { 
+    'version': 1,
+    'disable_existing_loggers': False, 
+    'filters': {
+        'require_debug_true': {
+        '()': 'django.utils.log.RequireDebugTrue',
+        }, 
+    }, 
+    'handlers': { 
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'], 'class': 'logging.StreamHandler',
+        }, 
+        'write_to_file': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'], 
+            'class': 'logging.FileHandler', 
+            'filename': 'db.log',
+        }, 
+    },
+    'loggers': { 
+        'django.db.backends': {
+            'handlers': ['console', 'write_to_file'],
+            'level': 'DEBUG',
+        },
+        'shop': {
+            'handlers': ['console'],
+            'level' : 'DEBUG',
+        },
+    }
+}
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
+import logging
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item
+
+logger = logging.getLogger(__name__) # __name__ => shop 내부의 뷰에 있다.
 
 def archives_year(request, year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
@@ -13,6 +16,7 @@ def item_list(request):
     if q:
         qs = qs.filter(name__icontains=q)
     
+    logger.debug('query : {}'.format(q))
     
     return render(request, 'shop/item_list.html', {
         'item_list' : qs,
