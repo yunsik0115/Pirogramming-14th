2cf17321ada5268e31388f9499ac2315782fb184
Yunsik Kim <pubyunsik0115@gmail.com>
2021-01-21 16:29:37 +0900

    class mistyped spelling, modificated

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from blog.models import Post
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin)
+class PostAdmin(admin.ModelAdmin):
     list_display = ('id','title','modified_at')
     list_filter = ('modified_at',)
     search_fields = ('title', 'content')
