e71def1883caec21abe31b7a3754063cbeae0cfa
Yunsik Kim <pubyunsik0115@gmail.com>
2021-01-23 01:12:29 +0900

    slug 에러 왜 안될까 생각해봐야할일

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from blog.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id','title','modified_at')
+    list_display = ('id', 'title', 'modified_at')
     list_filter = ('modified_at',)
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, re_path
 from blog import views
+from . import models
+from . import converters
+
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostLV.as_view(), name='post_detail'),
-    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+    path('', views.PostLV.as_view(), name='index'),
+    path('post/', views.PostLV.as_view(), name='post_list'),
+    path('<slug:slug>/', views.PostDV.as_view(), name='post_detail'),
     path('archive/', views.PostAV.as_view(), name='post_archive'),
     path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
     path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
