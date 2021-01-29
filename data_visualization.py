18750895eeedee1e3ab42df760c36c675acfcbf5
Yunsik Kim <pubyunsik0115@gmail.com>
2021-01-29 20:03:06 +0900

    data crawling for visualization completed

--- data_visualization.py
+++ data_visualization.py
@@ -4,30 +4,27 @@ import time
 import pandas as pd
 
 browser = webdriver.Chrome()
-url = "https://youtube-rank.com/board/bbs/board.php?bo_table=youtube"
 
-browser.get(url)
+results = []
 
-html = browser.page_source #Browser상의 브라우저 정보의 HTML소스 가져옴
-soup = BeautifulSoup(html, 'html.parser') # soup를 통해 문서 해석 및 데이터 추출 가능
+for page in range(1,11):
+    url = f"https://youtube-rank.com/board/bbs/board.php?bo_table=youtube&page={page}"
+    print(url)
 
-channel_list = soup.select('form > table > tbody > tr')
-print(len(channel_list), '\n')
-print(channel_list[0])
+    browser.get(url)
 
-channel = channel_list[0]
-print(channel)
+    html = browser.page_source  # Browser상의 브라우저 정보의 HTML소스 가져옴
+    soup = BeautifulSoup(html, 'html.parser')  # soup를 통해 문서 해석 및 데이터 추출 가능
 
-category = channel.select('p.category')[0].text.strip()
-print(category)
+    channel_list = soup.select('form > table > tbody > tr')
 
-title = channel.select('h1 > a')[0].text.strip()
-print(title)
+    for channel in channel_list:
+        category = channel.select('p.category')[0].text.strip()
+        title = channel.select('h1 > a')[0].text.strip()
+        subscriber = channel.select('.subscriber_cnt')[0].text
+        view = channel.select('.view_cnt')[0].text
+        video = channel.select('.video_cnt')[0].text
+        data = [title, category, subscriber, view, video]
+        results.append(data)
 
-subscriber = channel.select('.subscriber_cnt')[0].text
-view = channel.select('.view_cnt')[0].text
-video = channel.select('.video_cnt')[0].text
-
-print(subscriber)
-print(view)
-print(video)
+print(results)
